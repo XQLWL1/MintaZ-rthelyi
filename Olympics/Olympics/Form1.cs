02e8ee5e14d16000b1a247174c6490a76c43a1e2
XQLWL1 <vivien.sebestyen@stud.uni-corvinus.hu>
2021-11-29 15:08:26 +0100

    Data loading

--- Olympics/Olympics/Form1.cs
+++ Olympics/Olympics/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Olympics.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,48 @@ namespace Olympics
 {
     public partial class Form1 : Form
     {
+
+        List<OlympicResult> results = new List<OlympicResult>();
+
         public Form1()
         {
             InitializeComponent();
+
+            Betolt("Summer_olympic_Medals.csv");
         }
+
+
+        void Betolt(string fajlnev)
+        {
+
+           using (StreamReader streamReader = new StreamReader(fajlnev))
+            {
+                streamReader.ReadLine();
+
+                while (!streamReader.EndOfStream)
+                {
+                    string sor = streamReader.ReadLine();
+
+                    string[] mezok = sor.Split(',');
+
+                    OlympicResult olympicResult = new OlympicResult();
+                    olympicResult.Year = int.Parse(mezok[0]);
+                    olympicResult.Country = mezok[3];
+
+                    //Medálaknak 3 elemű tömböt kell létrehozni
+                    int[] medaltomb = new int[3];
+
+                    //5,6,7 lesz az arany, ezüt, bronz
+                    medaltomb[0] = int.Parse(mezok[5]);
+                    medaltomb[1] = int.Parse(mezok[6]);
+                    medaltomb[2] = int.Parse(mezok[7]);
+
+                    results.Add(olympicResult);
+                }
+
+            }
+
+        }
+
     }
 }
